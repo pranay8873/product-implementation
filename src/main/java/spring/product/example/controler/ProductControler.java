4a8c5ca22dfe4f93cecc87e9ef8c8a2a0090e332
pranay8873 <pranaypuvaati@gmail.com>
2026-06-12 23:08:50 +0530

    linking to postman

--- src/main/java/spring/product/example/controler/ProductControler.java
+++ src/main/java/spring/product/example/controler/ProductControler.java
@@ -2,35 +2,27 @@ package com.example.demo.controler;
 
 import com.example.demo.service.ProductService;
 import org.springframework.stereotype.Controller;
+import spring.product.example.model.Product;
 
 @Controller
 public class ProductControler {
-    private final ProductService productService;
-    public ProductControler(ProductService productService) {
-        this.productService = productService;
-    }
-    public void addProduct(String name, int id, String description, double price, String category, String manufacturer, String dateOfManufacture) {
-        productService.addProduct(name, id, description, price, category, manufacturer, dateOfManufacture);
-    }
-    public void updateProduct(int id, String name, String description, double price, String category, String manufacturer, String dateOfManufacture) {
-        productService.updateProduct(id, name, description, price, category, manufacturer, dateOfManufacture);
-    }
-    public void deleteProduct(int id) {
-        productService.deleteProduct(id);
-    }
-    public void getProductById(int id) {
-        productService.getProductById(id);
-    }
-    public void getProductByName(String name) {
-        productService.getProductByName(name);
-    }
-    public void findAll() {
-        productService.findAll();
-    }
-    public void deleteProductByName(String name) {
-        productService.deleteProductByName(name);
-    }
-
+   private final ProductService productService;
+   public ProductControler(ProductService productService) {
+       this.productService = productService;
+   }
+   @GetMapping("/addProduct")
+   public void addProduct() {
+       productService.addProduct(Product.builder().build());
+   }
+   @GetMapping("/saveProduct")
+   public ResponseEntity<Product> saveProduct() {
+       productService.saveProduct(Product.builder().build());
+       return ResponseEntity.ok().build();
+   }
+   @GetMapping("/getProductById")
+   public ResponseEntity<Product> getProductById() {
+       productService.getProductById();
+   }
 
 
 }
--- src/main/java/spring/product/example/service/productserviceimp.java
+++ src/main/java/spring/product/example/service/productserviceimp.java
@@ -1,4 +1,47 @@
 package spring.product.example.service;
 
-public class productserviceimp {
+import spring.product.example.model.Product;
+import spring.product.example.repository.Productrepos;
+
+import java.util.List;
+@Service
+public class productserviceimp implements com.example.demo.service.ProductService {
+    private final Productrepos productrepos;
+    public productserviceimp(Productrepos productrepos) {
+        this.productrepos = productrepos;
+    }
+    @Override
+    public void addProduct(String name, int id, String description, double price, String category, String manufacturer, String dateOfManufacture) {
+        productrepos.saveProduct(Product.builder().name(name).id(id).description(description).price(price).category(category).manufacturer(manufacturer).dateOfManufacture(dateOfManufacture).build());
+    }
+
+    @Override
+    public Product saveProduct(Product product) {
+        return productrepos.saveProduct(product);
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productrepos.getProductById(id);
+    }
+
+    @Override
+    public Product getProductByName(String name) {
+        return productrepos.getProductByName(name);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return productrepos.findAll();
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productrepos.deleteProduct(id);
+    }
+
+    @Override
+    public void deleteProductByName(String name) {
+        productrepos.deleteProductByName(name);
+    }
 }
