8f54d751c05c62a500bc4fca033ebbce0b8056aa
pranay8873 <pranaypuvaati@gmail.com>
2026-06-15 17:47:27 +0530

    entity class

--- src/main/java/com/product/project/entity/Product.java
+++ src/main/java/com/product/project/entity/Product.java
@@ -1,20 +1,14 @@
 package com.product.project.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
