cf1a08a1fd0308a61fd8eea013d167f348d2d078
pranay8873 <pranaypuvaati@gmail.com>
2026-06-16 16:16:21 +0530

    product responce and request classes added

--- src/main/java/com/product/project/controler/ProductCtlr.java
+++ src/main/java/com/product/project/controler/ProductCtlr.java
@@ -1,6 +1,9 @@
 package com.product.project.controler;
 
 import com.product.project.Exception.ProductExistsExcption;
+import com.product.project.dto.ProductRequestdto;
+import com.product.project.dto.ProductResponsedto;
+import com.product.project.dto.ProductUpdateDto;
 import com.product.project.entity.Product;
 import com.product.project.service.ProductServ;
 import lombok.RequiredArgsConstructor;
@@ -18,19 +21,19 @@ public class ProductCtlr {
     private final ProductServ productServ;
 
     @PostMapping("/add")
-    public ResponseEntity<Product> saveProduct(@RequestBody Product product) throws ProductExistsExcption {
+    public ResponseEntity<ProductResponsedto> saveProduct(@RequestBody ProductRequestdto product) throws ProductExistsExcption {
         return ResponseEntity.status(HttpStatus.CREATED).body(productServ.addProduct(product));
     }
     @GetMapping("/get/{id}")
-    public ResponseEntity<Product> getProductById(@PathVariable int id) {
+    public ResponseEntity<ProductResponsedto> getProductById(@PathVariable int id) {
         return ResponseEntity.ok(productServ.getProductById(id));
     }
     @GetMapping("/getByName")
-    public ResponseEntity<Optional<Product>> getProductByName(@RequestParam String name) {
-        return ResponseEntity.status(HttpStatus.OK).body(Optional.ofNullable(productServ.getProductByName(name)));
+    public ResponseEntity<ProductResponsedto> getProductByName(@RequestParam String name) {
+        return ResponseEntity.ok(productServ.getProductByName(name));
     }
     @GetMapping("/getByCategory")
-    public ResponseEntity<List<Product>> getProductByCategory(@RequestParam String category) {
+    public ResponseEntity<List<ProductResponsedto>> getProductByCategory(@RequestParam String category) {
         return ResponseEntity.ok(productServ.getProductByCategory(category));
     }
     @DeleteMapping("/delete/{id}")
@@ -38,17 +41,13 @@ public class ProductCtlr {
         productServ.deleteProduct(id);
     }
     @GetMapping("/getAll")
-    public ResponseEntity<List<Product>> getAllProducts() {
+    public ResponseEntity<List<ProductResponsedto>> getAllProducts() {
         return ResponseEntity.ok(productServ.getAllProducts());
     }
-    @PutMapping("/update")
-    public ResponseEntity<Optional<Product>> updateProduct(@RequestBody Product product) {
-        return ResponseEntity.ok(Optional.ofNullable(productServ.updateProduct(product)));
-    }
     @PutMapping("/update/{id}")
-    public ResponseEntity<Product> updateProductById(
+    public ResponseEntity<ProductResponsedto> updateProductById(
             @PathVariable int id,
-            @RequestBody Product product) {
+            @RequestBody ProductUpdateDto product) {
 
         product.setId(id);
         return ResponseEntity.ok(
