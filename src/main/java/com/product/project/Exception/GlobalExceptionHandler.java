54133a17f8cdc30b473d7692f7c6b6bf9fe03367
pranay8873 <pranaypuvaati@gmail.com>
2026-06-16 14:59:51 +0530

    exception handling and returning text msg to the user with conflit codes like 404 not found,etc

---
+++ src/main/java/com/product/project/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.product.project.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ProductExistsExcption.class)
+    public ResponseEntity<String> handleProductExistsException(ProductExistsExcption ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+    @ExceptionHandler(ProductNotFoundExcption.class)
+    public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundExcption ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+}
