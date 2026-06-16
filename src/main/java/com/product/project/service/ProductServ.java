5ddc25f89301fa95e9a610f8dc476fc814c7b268
pranay8873 <pranaypuvaati@gmail.com>
2026-06-16 16:17:15 +0530

    re configured the code according to the dto and dro object classes

--- src/main/java/com/product/project/service/ProductServ.java
+++ src/main/java/com/product/project/service/ProductServ.java
@@ -2,81 +2,101 @@ package com.product.project.service;
 
 import com.product.project.Exception.ProductExistsExcption;
 import com.product.project.Exception.ProductNotFoundExcption;
+import com.product.project.dto.ProductRequestdto;
+import com.product.project.dto.ProductResponsedto;
+import com.product.project.dto.ProductUpdateDto;
 import com.product.project.entity.Product;
-import com.product.project.repository.ProductReopo;
+import com.product.project.repository.ProductRepo;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServ implements ProductService {
-   private final ProductReopo productRepo;
+   private final ProductRepo productRepo;
+   private final ModelMapper modelMapper;
 
 
     @Override
-    public Product addProduct(Product product) throws ProductExistsExcption {
+    public ProductResponsedto addProduct(ProductRequestdto product) throws ProductExistsExcption {
         Optional<Product> product1 = productRepo.findByName(product.getName());
         if(product1.isPresent()) {
             throw new ProductExistsExcption("Product with name " + product.getName() + " already exists");
         }
-        return productRepo.save(product);
+        Product productentity=modelMapper.map(product, Product.class);
+        return modelMapper.map(productRepo.save(productentity),ProductResponsedto.class);
 
     }
 
     @Override
-    public Product getProductById(int id) throws ProductNotFoundExcption {
+    public ProductResponsedto getProductById(int id) throws ProductNotFoundExcption {
         Optional<Product> product1 = productRepo.findById(id);
         if(product1.isEmpty()) {
             throw new ProductNotFoundExcption("Product with id " + id + " not found");
         }
-        return product1.get();
+        return modelMapper.map(product1.get(),ProductResponsedto.class);
 
     }
     @Override
-    public Product getProductByName(String name) throws ProductNotFoundExcption {
+    public ProductResponsedto getProductByName(String name) throws ProductNotFoundExcption {
         Optional<Product> product1 = productRepo.findByName(name);
         if(product1.isEmpty()) {
             throw new ProductNotFoundExcption("Product with name " + name + " not found");
         }
-        return product1.get();
+        return modelMapper.map(product1.get(),ProductResponsedto.class);
 
     }
     @Override
-    public List<Product> getProductByCategory(String category) throws ProductNotFoundExcption {
-        Optional<Product> product1 = productRepo.findByCategory(category);
-        if(product1.isEmpty()) {
-            throw new ProductNotFoundExcption("Product with category " + category + " not found");
+    public List<ProductResponsedto> getProductByCategory(String category) throws ProductNotFoundExcption {
+        List<Product> products = productRepo.findByCategory(category);
+
+        if(products.isEmpty()) {
+            throw new ProductNotFoundExcption(
+                    "No products found in category " + category
+            );
         }
-        return Collections.singletonList(product1.get());
 
-    }
-    @Override
-    public List<Product> getAllProducts() {
-        return productRepo.findAll();
-    }
-
-    @Override
-    public Product updateProduct(Product product) throws ProductNotFoundExcption {
-        Optional<Product> product1 = productRepo.findById(product.getId());
-        if(product1.isEmpty()) {
-            throw new ProductNotFoundExcption("Product with id " + product.getId() + " not found");
-        }
-        return productRepo.save(product);
-
-
+        return products.stream()
+                .map(product -> modelMapper.map(product,
+                        ProductResponsedto.class))
+                .toList();
     }
 
     @Override
-    public boolean deleteProduct(int id) throws ProductNotFoundExcption {
+    public ProductResponsedto updateProduct(ProductUpdateDto product) throws ProductNotFoundExcption {
+        Product product1=productRepo.findById(product.getId()).orElseThrow(() -> new ProductNotFoundExcption("Product with id " + product.getId() + " not found"));
+        product1.setName(product.getName());
+        product1.setPrice(product.getPrice());
+        product1.setDescription(product.getDescription());
+        product1.setCategory(product.getCategory());
+        product1.setImage(product.getImage());
+        product1.setQuantity(product.getQuantity());
+        product1.setDiscount(product.getDiscount());
+        product1.setType(product.getType());
+        product1.setStatus(product.getStatus());
+        return modelMapper.map(productRepo.save(product1),ProductResponsedto.class);
+    }
+
+    @Override
+    public List<ProductResponsedto> getAllProducts() {
+        return productRepo.findAll().stream()
+                .map(product -> modelMapper.map(product, ProductResponsedto.class))
+                .collect(Collectors.toList());
+    }
+
+
+
+    @Override
+    public void deleteProduct(int id) throws ProductNotFoundExcption {
         if(productRepo.findById(id).isEmpty()) {
             throw new ProductNotFoundExcption("Product with id " + id + " not found");
         }
         productRepo.deleteById(id);
-        return true;
 
     }
 }
