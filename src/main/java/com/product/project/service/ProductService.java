caed7671f3c291857b41cdb5e896bfb64ebb6a3a
pranay8873 <pranaypuvaati@gmail.com>
2026-06-16 16:17:46 +0530

    improved repository naming conventions

--- src/main/java/com/product/project/service/ProductService.java
+++ src/main/java/com/product/project/service/ProductService.java
@@ -2,20 +2,32 @@ package com.product.project.service;
 
 import com.product.project.Exception.ProductExistsExcption;
 import com.product.project.Exception.ProductNotFoundExcption;
+import com.product.project.dto.ProductRequestdto;
+import com.product.project.dto.ProductResponsedto;
+import com.product.project.dto.ProductUpdateDto;
 import com.product.project.entity.Product;
 
 import java.util.List;
 
 public interface ProductService {
 
-    Product addProduct(Product product) throws ProductExistsExcption;
+    ProductResponsedto addProduct(ProductRequestdto product)
+            throws ProductExistsExcption;
 
-    Product getProductById(int id) throws ProductNotFoundExcption;
-    Product getProductByName(String name) throws ProductNotFoundExcption;
-    List<Product> getProductByCategory(String category) throws ProductNotFoundExcption;
+    ProductResponsedto getProductById(int id)
+            throws ProductNotFoundExcption;
 
-    Product updateProduct(Product product) throws ProductNotFoundExcption;
+    ProductResponsedto getProductByName(String name)
+            throws ProductNotFoundExcption;
 
-    boolean deleteProduct(int id) throws ProductNotFoundExcption;
-    List<Product> getAllProducts();
+    List<ProductResponsedto> getProductByCategory(String category)
+            throws ProductNotFoundExcption;
+
+    ProductResponsedto updateProduct(ProductUpdateDto product)
+            throws ProductNotFoundExcption;
+
+    void deleteProduct(int id)
+            throws ProductNotFoundExcption;
+
+    List<ProductResponsedto> getAllProducts();
 }
