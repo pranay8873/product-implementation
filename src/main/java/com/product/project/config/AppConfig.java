ef860d427b4a422a69023125ef0ed415f719072c
pranay8873 <pranaypuvaati@gmail.com>
2026-06-17 23:15:31 +0530

    added address entity to map tables 1-1,1-many,many-many,e.t.c

---
+++ src/main/java/com/product/project/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.product.project.config;
+
+import com.product.project.service.ProductServ;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ src/main/java/com/product/project/entity/Address.java
@@ -0,0 +1,10 @@
+package com.product.project.entity;
+
+public class Address {
+    String doorNo;
+    String street;
+    String city;
+    String state;
+    String country;
+    String pincode;
+}
