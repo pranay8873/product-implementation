4bba7d586572a5b8f11564e29be1421216966221
pranay8873 <pranaypuyaati@gmail.com>
2026-06-20 04:17:45 +0530

    updated address dto

---
+++ src/main/java/com/product/project/dto/addressdto.java
@@ -0,0 +1,13 @@
+package com.product.project.dto;
+
+import lombok.Data;
+
+@Data
+public class addressdto {
+    private String doorNo;
+    private String street;
+    private String city;
+    private String state;
+    private String country;
+    private String pincode;
+}
